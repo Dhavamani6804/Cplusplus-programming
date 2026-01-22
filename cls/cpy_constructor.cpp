1abf9c393dccba28c897182d83f23746805a28ae
Dhavamani6804 <sdhavamani22@gmail.com>
2026-01-22 11:15:22 +0530

    Refactor Student class: improve formatting and constructor implementation

--- cls/cpy_constructor.cpp
+++ cls/cpy_constructor.cpp
@@ -1,46 +1,45 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-class Student {
-private:
+ 
+class Student
+{
+    public:
+    //member variables
     int *p1;
     int *p2;
-
-public:
-    // Default Constructor
-    Student() {
-        cout << "Default Constructor Called" << endl;
-        p1 = new int(10);
-        p2 = new int(20);
+ 
+    //constructor
+    Student()
+    {
+        cout << " Default Constructor called." << endl;
+        p1 = new int(0);
+        *p1 = 10;
+        p2 = new int(0);
+        *p2 = 20;
     }
-
-    // Copy Constructor (Deep Copy)
-    Student(const Student &s) {
-        cout << "Copy Constructor Called" << endl;
-        p1 = new int(*s.p1);
-        p2 = new int(*s.p2);
+    Student(const Student &s)
+    {
+        cout << " Copy Constructor called." << endl;
+        p1 = new int;
+        *p1 = *(s.p1);
+        p2 = new int;
+        *p2 = *(s.p2);
     }
-
-    // Display function
-    void display() {
-        cout << "Value of p1: " << *p1 << endl;
-        cout << "Value of p2: " << *p2 << endl;
-    }
-
-    // Destructor
-    ~Student() {
-        delete p1;
-        delete p2;
-        cout << "Destructor Called" << endl;
+    void display()
+    {
+        cout << "Value at p1: " << *p1 << endl;
+        cout << "Value at p2: " << *p2 << endl;
     }
 };
-
-int main() {
-    Student s1;          // Default constructor
+ 
+int main()
+{
+    Student s1; //default constructor called
     s1.display();
-
-    Student s2 = s1;     // Copy constructor
+    Student s2 = s1; //copy constructor called
     s2.display();
-
+    *s1.p1 = 100;
+    s1.display();
+    s2.display();
     return 0;
 }
